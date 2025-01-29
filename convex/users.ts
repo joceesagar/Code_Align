9dbe4aa76f9c60c71ec8843234a929d0fd451956
joceesagar <joshisagarm2@gmail.com>
2025-01-29 11:09:02 +0545

    created logic for interview and comments

--- convex/users.ts
+++ convex/users.ts
@@ -1,6 +1,8 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
+
+//mutation in convex is used for creating or updating user
 export const syncUser = mutation({
     args: {
         name: v.string(),
@@ -16,5 +18,28 @@ export const syncUser = mutation({
             ...args, //...ars means all the properties of args like name, email etc
             role: "candidate"
         })
+    }
+})
+
+//query in convex is used for fetching data
+export const getUsers = query({
+    handler: async (ctx) => {
+        const identity = await ctx.auth.getUserIdentity();  //getting authenticated user identity
+        if (!identity) throw new Error("User is not authenticated")
+
+        const users = await ctx.db.query("users").collect()
+        return users
+    }
+})
+
+export const getUserByClerkId = query({
+    args: { clerkId: v.string(), },
+    handler: async (ctx, args) => {
+        const user = await ctx.db.
+            query("users")
+            .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId)) //it will loop through every user and return if the clerkid in db matches with the clerk id provided in arguments
+            .first()
+
+        return user
     }
 })
